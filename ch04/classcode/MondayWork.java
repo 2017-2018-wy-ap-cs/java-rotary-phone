1833693adff63d55013a89154b8f5661f0d66051
Doc MO <maueroats@gmail.com>
2018-01-16 08:57:20 -0600

    Chapter 12 StudentFiles

--- ch04/classcode/MondayWork.java
+++ ch04/classcode/MondayWork.java
@@ -1,13 +1,77 @@
 public class MondayWork 
 {
+  public static double q1(int n)
+  {
+    double answer = 0;
+    for(int k=1;k<=n; k++) {
+      answer += 1.0/(k*k);
+    }
+    return answer;
+  }
+
+  public static double q2(int n)
+  {
+    double answer = 0;
+    int sign = 1;
+    
+    for(int k=1; k<=n; k++) {
+      double nn = 1.0/(k*k);
+      answer += sign * nn;
+      sign *= -1;
+    }
+    return answer;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   public static double q5 (int n)
   {
     double answer = 1;
-    for (int k = 2; k <= n; k++) {
+    for (int k = 1; k <= n; k++) {
       answer = 1 + 1/answer;
     }
     return answer;
   }
+  
+  public static void q6(int n) 
+  {
+    int a = 1;
+    int b = 1;
+    
+    for (int k=2; k<= n; k++) {
+      int c = a+b;
+      a = b;
+      b = c;
+    }
+    
+    System.out.println(b + "/" + a);
+  }
+  
+    // HOMEWORK: 9, 10(a), 14
+   
+  
+  
+  
+  
+  
   
   
   public static double q5b (int n)
@@ -39,3 +103,4 @@ public class MondayWork
   }
   
 }
+
