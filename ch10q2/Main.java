4cd5b1edebd5551762e119504ddade3134b7217f
Doc MO <maueroats@gmail.com>
2018-01-20 18:41:15 -0600

    cleanup

--- ch10q2/Main.java
+++ ch10q2/Main.java
@@ -1,12 +1,7 @@
-import org.junit.*;
-import static org.junit.Assert.*;
-import junit.runner.Version;
 
 public class Main {
   public static void main (String[] args) {
     System.err.println("You should just hit the [Test Project] button in DrJava.");
-    System.err.println("JUnit version is: " + Version.id());
-    org.junit.runner.JUnitCore.main("HospitalTest");
+    org.junit.runner.JUnitCore.main("HospitalTest"); // MUST EDIT!
   }
-// Put your code in the Hospital.java file.
 }
